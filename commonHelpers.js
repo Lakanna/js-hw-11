9cb5005df0218c51e897ba6d50bad25ddd97d1e3
Lakanna <134102704+Lakanna@users.noreply.github.com>
2024-08-01 13:47:27 +0000

    Deploying to gh-pages from @ Lakanna/js-hw-11@cda747e3e651a8b9833304604b3a3ed00f8e0e31 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,13 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();function f(r){return r.map(({comments:o,likes:c,downloads:i,views:e,largeImageURL:t,webformatURL:n,tags:u})=>`
-          <li class="img-container">
-             <a href="${t}"><img src="${n}" alt="${u}" /></a>
-             <ul class="tags-list">
-               <li>Comments: ${o}</li>
-               <li>Likes: ${c}</li>
+import{i as l,S as u}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();function m(s){return s.map(({comments:t,likes:o,downloads:i,views:e,largeImageURL:r,webformatURL:a,tags:c})=>`
+        <li class="gallery-item">
+          <a href="${r}" class="img-container">
+             <img src="${a}" alt="${c}" />
+          </a>
+          <ul class="tags-list">
+               <li>Comments: ${t}</li>
+               <li>Likes: ${o}</li>
                <li>Views: ${e} </li>
                <li>Downloads: ${i}</li>
-             </ul>
-         </li>`).join("")}const l={searchForm:document.querySelector(".js-form"),imageList:document.querySelector(".js-list")};l.searchForm.addEventListener("submit",m);let s="";function m(r){if(r.preventDefault(),s=r.currentTarget.elements.searchImg.value.trim(),s===""){console.log("add value");return}p().then(({hits:o})=>l.imageList.innerHTML=f(o)).catch(o=>console.log(o)),console.log("in handler",s)}const d="40437222-3b8e1aead0ae08f3118e12752",h="https://pixabay.com/api/",g=new URLSearchParams({key:d,q:`${s}`,image_type:"photo",orientation:"horizontal",safesearch:"true"});console.log("not handler",s);const a=`${h}?${g}`;console.log(a);function p(){return fetch(a).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}
+          </ul>
+        </li>`).join("")}const d="40437222-3b8e1aead0ae08f3118e12752",f="https://pixabay.com/api/";function h(s){const t=new URLSearchParams({key:d,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});let o=`${f}?${t}`;return fetch(o).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()})}const n={searchForm:document.querySelector(".js-form"),imageList:document.querySelector(".js-list"),loader:document.querySelector(".loader")};n.searchForm.addEventListener("submit",g);function g(s){s.preventDefault();let t=s.currentTarget.elements.searchImg.value.trim();if(t===""){l.show({message:"Please, input what are you searching!",color:"red",position:"topCenter"});return}n.loader.hidden=!1,console.dir(n.loader),h(t).then(({hits:o})=>{o.length===0&&l.show({message:"Sorry, there are no images matching your search query. Please try again!",color:"red",position:"topCenter"}),n.imageList.innerHTML=m(o),new u(".image-list a").refresh()}).catch(o=>console.log(o)),n.loader.hidden=!0,s.target.reset()}
 //# sourceMappingURL=commonHelpers.js.map
